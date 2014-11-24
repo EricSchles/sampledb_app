e20a4109a2b786c31713fb346f1328e5e0511bff
EricSchles <ericschles@gmail.com>
2014-11-24 10:44:38 -0800

    testing

---
+++ flaskr.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config["DATABASE"] = 'database.db'
+db = SQLALchemy
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+
+    def __init__(self,name):
+        self.name = name
+
+    def __repr__(self):
+        return '<User %r>' % self.name
+
